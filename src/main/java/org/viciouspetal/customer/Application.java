212ecfdcba39f17c3e5390e0af891e8416629ff1
joanna.wojcik <joanna.wojcik@hotmail.com>
2018-05-20 19:55:53 +0100

    trying to get eureka to map to proper app url2

--- src/main/java/org/viciouspetal/customer/Application.java
+++ src/main/java/org/viciouspetal/customer/Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @EnableDiscoveryClient
+@EnableEurekaClient
 @EnableAutoConfiguration
 @SpringBootApplication
 public class Application {
