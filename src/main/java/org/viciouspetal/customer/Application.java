2f4ce630848315e88c53f932ea43005b363c7ddd
joanna.wojcik <joanna.wojcik@hotmail.com>
2018-05-20 20:49:48 +0100

    eureka experiment

--- src/main/java/org/viciouspetal/customer/Application.java
+++ src/main/java/org/viciouspetal/customer/Application.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @EnableEurekaClient
 @EnableAutoConfiguration
 @SpringBootApplication
