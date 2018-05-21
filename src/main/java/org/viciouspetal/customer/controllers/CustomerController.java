72406a1ee86746c01fe061f06e220dc381f4074b
joanna.wojcik <joanna.wojcik@hotmail.com>
2018-05-21 19:52:57 +0100

    removing cors

--- src/main/java/org/viciouspetal/customer/controllers/CustomerController.java
+++ src/main/java/org/viciouspetal/customer/controllers/CustomerController.java
@@ -14,25 +14,22 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @CrossOrigin(origins = "https://assignment-ui.cfapps.io")
+
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public List<Customer> getList() {
         return customerService.list();
     }
 
-    @CrossOrigin(origins = "https://assignment-ui.cfapps.io")
     @RequestMapping(value = "/create", method = RequestMethod.POST)
     public Customer create(@RequestBody Customer customer){
         return customerService.create(customer);
     }
 
-    @CrossOrigin(origins = "https://assignment-ui.cfapps.io")
     @RequestMapping(value = "/update/{id}", method = RequestMethod.POST)
     public Customer update(@PathVariable String id, @RequestBody Customer object) {
         return customerService.update(object);
     }
 
-    @CrossOrigin(origins = "https://assignment-ui.cfapps.io")
     @RequestMapping(value = "/delete/{id}", method = RequestMethod.DELETE)
     public void delete(@PathVariable String id) {
         customerService.delete(id);
